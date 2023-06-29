f93be3ef629a1610fdef7ff3a7c527ecde4a5b68
cbot918 <cbot918@gmail.com>
2023-06-29 16:22:15 +0800

    integrate sqlc and app.go

--- server/mock/mockdb.go
+++ server/mock/mockdb.go
@@ -1,7 +1,27 @@
 package mock
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/cbot918/grpost/server/util"
+	_ "github.com/lib/pq"
+)
 
 func MockDb() {
 	fmt.Println("hihi")
+
+	config, err := util.LoadConfig(".", "app", "env")
+	if err != nil {
+		fmt.Println("failed to load config")
+	}
+	fmt.Println(config.DSN)
+
+	// connPoll, err := sql.Open("postgres", )
+
+	// db.New()
+}
+
+func main() {
+
+	MockDb()
 }
